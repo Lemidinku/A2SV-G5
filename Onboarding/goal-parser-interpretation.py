5686706dc044ea93e9f024d62baf5531f760455c
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2023-11-30 18:55:42 +0300

    Added solution for Goal Parser Interpretation

---
+++ Onboarding/goal-parser-interpretation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        
+        parsed = ""
+        i = 0
+        while i < len(command):
+            if command[i]=="G":
+                parsed += "G"
+                i+=1
+            elif command[i:i+2] == "()":
+                parsed += "o"
+                i+=2
+            else:
+                parsed += "al"
+                i+=4
+        return parsed
+
