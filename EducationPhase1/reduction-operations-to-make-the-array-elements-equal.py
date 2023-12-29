d953f4867e8a0d85639b7c04751090ceea168545
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2023-12-29 18:14:30 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

--- EducationPhase1/reduction-operations-to-make-the-array-elements-equal.py
+++ EducationPhase1/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,7 +1,17 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         
-        nums.sort()
+        # nums.sort()
+        maxx  = max(nums)+1
+        count = [0]*maxx
+        for num in nums:
+            count[num] +=1
+        
+        nums = []
+        for i in range(maxx):
+            if count[i]:
+                nums += [i]*count[i]
+        
         prev = nums[0]
         ops = 0
         tot_ops = 0
