43efc45c064f64981a586c3a6f6b4c2d668420aa
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2023-12-29 16:52:19 +0300

    Added solution for Sorting the Sentence

---
+++ EducationPhase1/sorting-the-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s = list(s.split())
+        s.sort(key= lambda x: x[-1])
+
+        s = [ word[:-1] for word in s]
+
+        return " ".join(s)
+        
