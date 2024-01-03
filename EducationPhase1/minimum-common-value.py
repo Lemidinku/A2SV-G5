2a5a51a5b3954f479d1df5d278103dd0dcc60da5
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-01-03 18:01:14 +0300

    Added solution for Minimum Common Value

---
+++ EducationPhase1/minimum-common-value.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        top = bottom = 0
+
+        while top<len(nums1) and bottom < len(nums2):
+            if nums1[top] == nums2[bottom]:
+                return nums1[top]
+            elif nums1[top] < nums2[bottom]:
+                top +=1
+            else:
+                bottom +=1
+        
+        return -1
+
