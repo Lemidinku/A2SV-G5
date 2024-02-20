194f5c0969234649a4c44dd2838eccf94e5a9dff
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-02-20 21:04:47 +0300

    Add solution for Reverse String

---
+++ camp1/leetcode/reverse-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        def rev(s,left,right):
+            if left>=right:
+                return 
+            
+            s[left],s[right] = s[right],s[left]
+            rev(s,left+1,right-1)
+
+        rev(s, 0, len(s)-1)
