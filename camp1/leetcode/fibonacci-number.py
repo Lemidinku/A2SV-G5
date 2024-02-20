5ab068b35e060ba86dc23c8636d36fd96f1c28e2
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-02-20 21:02:19 +0300

    Add solution for Fibonacci Number

---
+++ camp1/leetcode/fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self):
+        self.cache = {}
+    def fib(self, n: int) -> int:
+        
+        if n<2:
+            return n
+        elif n in self.cache:
+            return self.cache[n]
+        
+        ans = self.fib(n-1) + self.fib(n-2)
+        self.cache[n]=ans
+        return ans
