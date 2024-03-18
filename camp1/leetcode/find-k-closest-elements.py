fb11d8fdfa81b0b488f50f546cf62e0c23dda4b9
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-03-18 16:38:51 +0300

    Add solution for Find K Closest Elements

--- camp1/leetcode/find-k-closest-elements.py
+++ camp1/leetcode/find-k-closest-elements.py
@@ -1,6 +1,9 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        
-        diffs = sorted(arr, key=lambda a:abs(a-x))
-
-        return sorted(diffs[:k])
+        left, right = 0, len(arr) - 1
+        while right - left >= k:
+            if arr[right] - x <  x-arr[left]:
+                left += 1
+            else:
+                right -= 1
+        return arr[left : right + 1]
