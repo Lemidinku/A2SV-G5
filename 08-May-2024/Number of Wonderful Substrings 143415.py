d0619e6e4953a12df4bc9a9d40f2e742a5306eb4
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-05-08 17:42:07 +0300

    Created 'Number of Wonderful Substrings' by A2SV Hub

---
+++ 08-May-2024/Number of Wonderful Substrings 143415.py
@@ -0,0 +1,19 @@
+# Problem: Number of Wonderful Substrings - https://leetcode.com/problems/number-of-wonderful-substrings/
+
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+
+        freq = defaultdict(int)
+        freq[0] = 1
+        count = 0
+        xor = 0
+        for char in word:
+            val = ord(char)-ord("a")
+            xor ^= (1<<val)
+            count += freq[xor]
+            for i in range(10):
+                num = xor^(1<<i)
+                count += freq[num]
+            freq[xor]+=1
+
+        return count
