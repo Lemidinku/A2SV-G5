1f53d114368c6a2cebb92f75949060defe86c765
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-05-10 17:22:49 +0300

    Created 'Target Sum' by A2SV Hub

---
+++ 10-May-2024/Target Sum 145701.py
@@ -0,0 +1,18 @@
+# Problem: Target Sum - https://leetcode.com/problems/target-sum/
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp= {}
+        self.count = 0
+        def backtrack(i,total):
+            if (i,total) in dp:
+                return dp[(i,total)]
+            if i == len(nums):
+                if total == target:
+                    return  1
+                return 0
+            dp[(i,total)] = backtrack(i+1,total + nums[i]) + backtrack(i+1,total - nums[i])
+            return dp[(i,total)]
+        
+        return backtrack(0,0)
+
