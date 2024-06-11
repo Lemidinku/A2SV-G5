913ca7f569ae8b5ca0af3b19775155678675f8c9
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-06-11 13:53:38 +0300

    Created 'First Missing Positive' by A2SV Hub

---
+++ 11-Jun-2024/First Missing Positive 166715.py
@@ -0,0 +1,23 @@
+# Problem: First Missing Positive - https://leetcode.com/problems/first-missing-positive/description/
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        valid_for = [1, 7, 30]
+        n = len(days)
+        @cache
+        def backtrack(i, ends):
+            if i>= n:
+                return 0
+
+
+            money_needed = float('inf')
+            if days[i]>=ends:
+                for x in range(3):
+                    money_needed = min(money_needed,backtrack(i+1, days[i]+valid_for[x])+ costs[x]) 
+            else:
+                money_needed = min(money_needed,backtrack(i+1, ends))
+               
+            return money_needed 
+
+        return backtrack(0,0)
