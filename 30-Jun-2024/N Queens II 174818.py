f4fc440ffd6149ae8cd60b6cee6b7908774a171f
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-06-30 07:02:12 +0300

    Created 'N Queens II' by A2SV Hub

---
+++ 30-Jun-2024/N Queens II 174818.py
@@ -0,0 +1,42 @@
+# Problem: N Queens II - https://leetcode.com/problems/n-queens-ii/
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        attacked_cols = set()
+        attacked_diagonals1 = set()
+        attacked_diagonals2 = set()
+        boards = []
+        curr_board = [["."]*n for _ in range(n)]
+
+
+
+        def valid(r,c):
+            if c in attacked_cols: return False
+            if r-c in attacked_diagonals1: return False
+            if r+c in attacked_diagonals2: return False
+            return True
+        def removeQueen(r,c):
+            attacked_cols.remove(c)
+            attacked_diagonals1.remove(r-c)
+            attacked_diagonals2.remove(r+c)
+            curr_board[r][c] = "."
+        def placeQueen(r,c):
+            attacked_cols.add(c)
+            attacked_diagonals1.add(r-c)
+            attacked_diagonals2.add(r+c)
+            curr_board[r][c] = "Q"
+
+
+        def backtrack(i):
+            if i==n:
+                a = ["".join(row) for row in curr_board ]
+                if a not in boards:
+                    boards.append(a)
+                return
+            for col in range(n):
+                if valid(i,col):
+                    placeQueen(i,col)
+                    backtrack(i+1)
+                    removeQueen(i,col)
+        backtrack(0)
+        return len(boards)
