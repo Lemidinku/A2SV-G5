12d3d59143e3d67aebd62025cfe15cf3294fe52f
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-01 10:43:26 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 01-Aug-2024/Jump Game 186477.py
@@ -0,0 +1,15 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        curr = 0
+        for a in range(len(nums)-1):
+            curr-=1
+            curr = max(curr,nums[a])
+            if curr==0: 
+                return False
+                
+        return True
+
+
+        
