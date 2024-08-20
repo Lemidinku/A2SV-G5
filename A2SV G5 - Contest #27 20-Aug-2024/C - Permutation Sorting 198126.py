0591ce1c4b27ea53150d12188580e91721a46599
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-20 17:46:58 +0300

    Created 'C - Permutation Sorting' by A2SV Hub

---
+++ A2SV G5 - Contest #27 20-Aug-2024/C - Permutation Sorting 198126.py
@@ -0,0 +1,15 @@
+# Problem: C - Permutation Sorting - https://codeforces.com/gym/538762/problem/C
+
+t = int(input())
+for _ in range(t):
+    n  = int(input())
+    arr = list(map(int, input().split()))
+
+    nums = {arr[i]:i for i in range(n)}
+
+    correct = 0
+
+    for i in range(1,n):
+        if nums[i] < nums[i+1]:
+            correct += 1
+    print(n-1-correct)
