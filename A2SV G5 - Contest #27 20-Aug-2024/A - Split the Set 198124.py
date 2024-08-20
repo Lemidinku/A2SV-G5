362fd2c273f16584e1f3e2d034ccaaa03a4c9d91
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-20 17:46:38 +0300

    Created 'A - Split the Set' by A2SV Hub

---
+++ A2SV G5 - Contest #27 20-Aug-2024/A - Split the Set 198124.py
@@ -0,0 +1,18 @@
+# Problem: A - Split the Set - https://codeforces.com/gym/538762/problem/A
+
+t = int(input())
+for _ in range(t):
+    n  = int(input())
+    arr = list(map(int, input().split()))
+
+    odds = 0
+    evens  =0
+    for a in arr:
+        if a%2:
+            odds+=1
+        else: evens +=1
+    
+    if odds >= n and evens >= n:
+        print('Yes')
+    else:
+        print("No")
