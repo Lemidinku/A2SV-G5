6a259ae0af66d7709a9ce2f9351f355f7e3ee4ab
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-20 17:46:52 +0300

    Created 'B - Fasika and Children' by A2SV Hub

---
+++ A2SV G5 - Contest #27 20-Aug-2024/B - Fasika and Children 198125.py
@@ -0,0 +1,14 @@
+# Problem: B - Fasika and Children - https://codeforces.com/gym/538762/problem/B
+
+n, m = map(int, input().split())
+import math
+arr = list(map(int, input().split()))
+
+nums = [math.ceil(a/m) for a in arr]
+
+ans = 0
+for i in range(n):
+    if nums[i] >= nums[ans]:
+        ans = i
+print(ans+1)
+
