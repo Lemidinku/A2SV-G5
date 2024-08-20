9060ba0fff0da7f0726a83931582bd66512f6f78
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-20 22:32:15 +0300

    Created 'D - Candies in the Box' by A2SV Hub

---
+++ A2SV G5 - Contest #27 20-Aug-2024/D - Candies in the Box 198246.py
@@ -0,0 +1,26 @@
+# Problem: D - Candies in the Box - https://codeforces.com/gym/538762/problem/D
+
+import math
+n  = int(input())
+def eatsHalf(n,k):
+    initial = n
+    value =0
+    turn = 0
+    while n:
+        if not turn:
+            value += min(n,k)
+            n -= min(n,k)
+        else:
+            n -= (n//10)
+        turn  = 1-turn
+    return 2*value >= initial
+ 
+left, right = 1, n
+while left <= right:
+    mid = left + (right-left)//2
+    if eatsHalf(n,mid):
+        right = mid-1
+    else:
+        left = mid+1
+print(left)
+ 
