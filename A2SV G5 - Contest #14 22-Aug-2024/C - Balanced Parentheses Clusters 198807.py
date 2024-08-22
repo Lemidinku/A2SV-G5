3ccb7e962e39bc85633ca93f5f4e167d05c28903
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-22 23:07:09 +0300

    Created 'C - Balanced Parentheses Clusters' by A2SV Hub

---
+++ A2SV G5 - Contest #14 22-Aug-2024/C - Balanced Parentheses Clusters 198807.py
@@ -0,0 +1,12 @@
+# Problem: C - Balanced Parentheses Clusters - https://codeforces.com/gym/520688/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    n *=2
+    word = input()
+    count = 0
+    for i in range(n-1):
+        if word[i]==word[i+1]=="(":
+            count +=1
+    print(count+1)
