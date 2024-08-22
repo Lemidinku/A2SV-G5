61a4d82e86c0855247a739c5405509b47a2a9fa0
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-22 23:03:24 +0300

    Created 'E - XOR Fan' by A2SV Hub

---
+++ A2SV G5 - Contest #15 22-Aug-2024/E - XOR Fan 198795.py
@@ -0,0 +1,37 @@
+# Problem: E - XOR Fan - https://codeforces.com/gym/522079/problem/E
+
+t = int(input())
+for _ in range(t):
+    n  = int(input())
+    nums = list(map(int, input().split()))
+    s = input()
+
+    prefix = [0]
+    for number in nums:
+        prefix.append(prefix[-1] ^ number)
+    zeros = 0
+    ones = 0
+    for i in range(n):
+        if s[i]=="1":
+            ones ^= nums[i]
+        else:
+            zeros ^= nums[i]
+    
+    q = int(input())
+    ans = []
+    for _ in range(q):
+        ops = list(map(int, input().split()))
+        if ops[0]==1:
+            _, left, right = ops
+            rang = prefix[right]^prefix[left-1]
+            zeros ^= rang
+            ones ^= rang
+        else:
+            _, g = ops
+            if g:
+                ans.append(ones)
+            else:
+                ans.append(zeros)
+    print(*ans)
+
+
