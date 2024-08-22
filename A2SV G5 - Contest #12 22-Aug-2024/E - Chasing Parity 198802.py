b860cc3be29245e5feeb2bf3ca8789577563ff3d
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-22 23:05:17 +0300

    Created 'E - Chasing Parity' by A2SV Hub

---
+++ A2SV G5 - Contest #12 22-Aug-2024/E - Chasing Parity 198802.py
@@ -0,0 +1,40 @@
+# Problem: E - Chasing Parity - https://codeforces.com/gym/517685/problem/E
+
+from collections import Counter, defaultdict, deque
+
+n =int(input())
+arr = list(map(int, input().split()))
+graph = defaultdict(list)
+for i in range(n):
+    if i+arr[i] < n:
+        graph[i+arr[i]].append(i)
+    if i-arr[i] >=0:
+        graph[i-arr[i]].append(i)
+
+
+result = [-1]*n
+
+def bfs(parity):
+    queue  = deque()
+    visited  = set()
+    for i in range(n):
+        if arr[i]%2==parity:
+            visited.add(i)
+            queue.append(i)
+    dis = 0
+    while queue:
+        for _ in range(len(queue)):
+            curr = queue.popleft()
+
+            if arr[curr]%2!= parity:
+                result[curr] = dis
+
+            for neighbour in graph[curr]:
+                if neighbour not in visited:
+                    queue.append(neighbour)
+                    visited.add(neighbour)
+        dis +=1
+bfs(0)
+bfs(1)
+print(*result)
+
