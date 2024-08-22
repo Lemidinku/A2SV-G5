cc10f9f70bf729ad5c2f2258e5daf0390f30cd49
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-23 00:22:20 +0300

    Created 'B - Excluded Integer Sum Problem' by A2SV Hub

---
+++ A2SV G5 - Contest #22 22-Aug-2024/B - Excluded Integer Sum Problem 198849.py
@@ -0,0 +1,34 @@
+# Problem: B - Excluded Integer Sum Problem - https://codeforces.com/gym/531455/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k, x = map(int, input().split())
+
+    def possible():
+        if x!=1:
+            print("YES")
+            print(n)
+            print(*[1]*n)
+        else:
+            if k==1:
+                print("NO")
+                return
+            if k==2:
+                if n%2:
+                    print("NO")
+                else:
+                    print("YES")
+                    print(n//2)
+                    print(*[2]*(n//2))
+            else:
+                print("YES")
+                if not n%2:
+                    print(n//2)
+                    print(*[2]*(n//2))
+                else:
+                    print(n//2)
+                    print(*([2]*(n//2-1)+[3]))
+
+    possible()
+
+
