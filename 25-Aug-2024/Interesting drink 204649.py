7fd661d2f99629b0a63a198af7659954c18b1baa
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-25 21:40:04 +0300

    Created 'Interesting drink' by A2SV Hub

---
+++ 25-Aug-2024/Interesting drink 204649.py
@@ -0,0 +1,11 @@
+# Problem: Interesting drink - https://codeforces.com/problemset/problem/706/B/
+
+from bisect import bisect_right
+n  = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+q = int(input())
+for i in range(q):
+    x = int(input())
+    print(bisect_right(arr, x))
+    
