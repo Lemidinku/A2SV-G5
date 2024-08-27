44e7ffaa3c5f19de620f475cffa8d480f7086308
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-08-27 22:28:43 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 27-Aug-2024/New Year Transportation 208440.py
@@ -0,0 +1,20 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, x = map(int, input().split())
+arr = list(map(int, input().split()))
+
+
+def solve():
+    curr = 1
+    while curr < x:
+        curr += arr[curr-1]
+        if curr == x:
+            print("YES")
+            return
+    print("NO")
+
+
+solve()
+
+
+    
