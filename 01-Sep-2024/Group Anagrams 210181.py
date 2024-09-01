bd0e5d3704018fc28af98766386f098b80edbe94
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2024-09-01 21:55:12 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 01-Sep-2024/Group Anagrams 210181.py
@@ -0,0 +1,11 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+from collections import defaultdict, Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        table = defaultdict(list)
+        for word in strs:
+            letters = "".join(sorted([x for x in word]))
+            table[letters].append(word)
+        
+        return [x for x in table.values()]
